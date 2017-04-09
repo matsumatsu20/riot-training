710272703561154d0a9cbd1f784c6043e23fd9ce
Yuki Matsuyama <matsumatsu20@gmail.com>
2017-04-10 00:14:24 +0900

    mixinの記述の削除

--- src/js/index.js
+++ src/js/index.js
@@ -5,26 +5,4 @@ import './riot/app'
 import './riot/main'
 import './riot/archive'
 
-const todoItems = [{ title: 'default item', description: 'This is default todo.', done: true }]
-const archiveItems = []
-
-const renderTodoItems = () => todoItems
-const renderArchiveItems = () => archiveItems
-
-riot.mixin('todoItems', {
-  renderTodoItems: renderTodoItems,
-  renderArchiveItems: renderArchiveItems
-})
-
-riot.observable(todoItems)
-riot.observable(archiveItems)
-
-todoItems.on('refresh', () => {
-  riot.update()
-})
-
-archiveItems.on('refresh', () => {
-  riot.update()
-})
-
 riot.mount('app')
