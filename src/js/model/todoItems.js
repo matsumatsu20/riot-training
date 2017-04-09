ad55c4fb67361443cee6d93588cd3a921a60ddd3
Yuki Matsuyama <matsumatsu20@gmail.com>
2017-04-10 00:15:23 +0900

    アーカイブを完全に削除する機能を作成した（ゴミ箱を空にするようなイメージ）

--- src/js/model/todoItems.js
+++ src/js/model/todoItems.js
@@ -1,4 +1,7 @@
-const todoItems = [{ title: 'default item', description: 'This is default todo.', done: true, archive: false }]
+const todoItems = [
+  { title: 'default item', description: 'This is default todo.', done: true, archive: false },
+  { title: 'default archive item1', description: 'This is default archive item.', done: true, archive: true }
+]
 
 riot.observable(todoItems)
 
